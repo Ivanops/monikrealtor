91d03b8a4ff1a27903373eab57dfc2b63c0e05d8
Ivan <ivanops44@gmail.com>
2015-11-19 22:20:14 -0500

    fix for openshift

--- server.js
+++ server.js
@@ -1,12 +1,16 @@
 var express = require('express');
-var app = express();
+var server = express();
 
-app.use(express.static(__dirname + '/public'));
+server.use(express.static(__dirname + '/public'));
 
-app.get('/', function(req, res) {
+var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
+var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
+ 
+server.listen(server_port, server_ip_address, function () {
+  console.log( "Listening on " + server_ip_address + ", server_port " + port )
+});
+
+server.get('/', function(req, res) {
 	res.send('Hello World!');
 });
 
-app.listen(3000, function() {
-	console.log('app listen on 3000');
-})
